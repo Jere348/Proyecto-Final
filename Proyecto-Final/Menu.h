6885d33edec28e99338169b3463ee61381a32462
Jere348 <rodrisoto987@gmail.com>
2025-07-06 17:27:02 -0500

    New

--- Proyecto-Final/Menu.h
+++ Proyecto-Final/Menu.h
@@ -65,54 +65,7 @@ string leerContrasenaOculta(short x, short y) {
 }
 
 
-string generarContraseña(int longitud = 8) {
-    string caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    string pass;
-    for (int i = 0; i < longitud; ++i) {
-        pass += caracteres[rand() % caracteres.size()];
-    }
-    return pass;
-}
 
-int generarTelefono() {
-    int telefono = 9 * 100000000;
-    for (int i = 0; i < 8; ++i) {
-        telefono += (rand() % 10) * pow(10, 7 - i);
-    }
-    return telefono;
-}
-
-// Generar un solo usuario
-User* generarUsuario(int id) {
-    vector<string> nombres = { "Luis", "Ana", "Carlos", "María", "Pedro", "Lucía", "Diego", "Sofía", "Jorge", "Elena" };
-    vector<string> apellidos = { "Gómez", "Pérez", "Ramírez", "Torres", "Sánchez", "Díaz", "Castro", "Vargas", "Mendoza", "Ríos" };
-    vector<string> distritos = { "SanMiguel", "Barranco", "SanIsidro" };
-
-    string nombre = nombres[rand() % nombres.size()] + " " + apellidos[rand() % apellidos.size()];
-
-    string correo = nombre;
-    replace(correo.begin(), correo.end(), ' ', '.');
-    transform(correo.begin(), correo.end(), correo.begin(), ::tolower);
-    correo += to_string(id) + "@correo.com";
-
-    string contraseña = generarContraseña();
-    string distrito = distritos[rand() % distritos.size()];
-    int telefono = generarTelefono();
-
-    return new User(nombre, telefono, distrito, correo, contraseña);
-}
-
-
-void cargarDataset() {
-    srand(time(0));
-    AVLArbol<User> UserFromDataSet;
-    User* usuario = new User();
-
-    for (int i = 0; i < 100; ++i) {
-        usuario = generarUsuario(i);
-        UserFromDataSet.insertar(usuario);
-    }
-}
 
 class Controlador
 {
